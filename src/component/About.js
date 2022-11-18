b79aa7f5134c44239385f2ab52255be6d35fdd8b
RaghadSuwan <raghadsu979@>
2022-11-18 13:26:33 +0200

    little editing

--- src/component/About.js
+++ src/component/About.js
@@ -1,46 +1,80 @@
 //import React, { Component } from 'react';
+
+
 function About(){
     return(
      <>
- 
-     <section class="about">
-         <div class="main">
-         <div class="about-text"> 
-                 <div>
-                     <h1>About us</h1>
-                 </div>
-                 <div>
-                     <h4>Why Was Established?</h4>
-                     <p> Cash financial transactions control the economy in Palestine.
-                         The ratio of Palestinians who have bank accounts is about 30% of 
-                         the number of citizens eligible for opening bank accounts. 
-                         This results in 95% of financial transactions being carried out in 
-                         cash resulting in increasing the value and cost of cash itself! 
-                         This imposed a heavy burden on the Palestinian economy represented by its banks, traders, and citizens.
-                         Hence, Jawwal Pay will fill the vacuum of absent electronic payment services 
-                         urgently needed by the Palestinian market, through innovative electronic payment systems 
-                         to transfer the national economy into an advanced digital economy
-                     </p>
-                 </div>
-                 <div>
-                     <h4>Who Benefits from Services? </h4>
-                     <p>
-                         Palestenian individuals will benefit . 
-                         Electronic payment and wallet services will contribute to a large extent to 
-                         reducing cost for all sectors of society in addition to providing comfort, convenience, a
-                         nd time for users.
-                     </p>
-                 </div>
-                 <div>
-                     <h4>Our Vision and Mission</h4>
-                     <p>
-                         aims at leading the Palestinian society and transferring it into a non-cash 
-                         society by providing easy and comfortable electronic access and channels to
-                         electronic payment systems that are innovative, developed, safe, and secure at an affordable cost.
-                     </p>
-                 </div>
-         </div>
-         </div>
+     <section className="about">
+         <div className="main">
+            <div class="container">
+                <div class ="row">
+                    <div class="col-md"> 
+                        <div className="image">
+                            <img src="/image/ewallet-icone.png"  alt="about the website"/>
+                                        {/* //<img src="/image/crypto-wallet.png" id="img2" alt="about the website"/> */}
+                        </div>
+                        <div className="about-us">
+                            <h1><b>About us</b></h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+         <div className="about-text"> 
+         <div class="container">
+            <div class ="row">
+                <div class="col-md"> 
+                    <div>
+                        {/* <img src="/image/551fffffffff-01.png"  alt="about the website"/> */}
+                    </div>
+                    <div className="text1">
+                        <h4><b>Why Was Established?</b> </h4>
+                        <p> Cash financial transactions control the economy in Palestine.
+                            The ratio of Palestinians who have bank accounts is about 30% of 
+                            the number of citizens eligible for opening bank accounts. 
+                            This results in 95% of financial transactions being carried out in 
+                            cash resulting in increasing the value and cost of cash itself! 
+                            This imposed a heavy burden on the Palestinian economy represented by its banks, traders, and citizens.
+                            Hence, Jawwal Pay will fill the vacuum of absent electronic payment services 
+                            urgently needed by the Palestinian market, through innovative electronic payment systems 
+                            to transfer the national economy into an advanced digital economy.
+                        </p>
+                    </div>   
+                </div>
+            </div>
+        </div>
+                 
+        <div class="container">
+                <div class ="row">
+                    <div class="col-md"> 
+                        <div className="text1">
+                        <h4 > <b>Who Benefits from Services? </b></h4>
+                        <p>
+                            Palestenian individuals will benefit . 
+                            Electronic payment and wallet services will contribute to a large extent to 
+                            reducing cost for all sectors of society in addition to providing comfort, convenience, a
+                            nd time for users.
+                        </p>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        <div class="container">
+                <div class ="row">
+                    <div class="col-md"> 
+                        <div className="text1">
+                            <h4 > <b>Our Vision and Mission</b></h4>
+                            <p>
+                                aims at leading the Palestinian society and transferring it into a non-cash 
+                                society by providing easy and comfortable electronic access and channels to
+                                electronic payment systems that are innovative, developed, safe, and secure at an affordable cost.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
    </section>
  </>
  );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';//import react library 
+import React from 'react';              //import react library 
 import ReactDOM from 'react-dom/client';//import reactdom library 
-import App from './App';//import main file (App) all work on it
+import App from './App';                //import main file (App) all work on it
 import About from './component/About';
-//render is a function display app file 
+ //render is a function display app file 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
