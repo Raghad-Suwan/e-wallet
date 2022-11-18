ae63d402120719b39e124e8f220c51530e596586
lara-taweel <s12115919@stu.najah.edu>
2022-11-19 02:07:08 +0300

    transmoney

--- src/components/Cardd.js
+++ src/components/Cardd.js
@@ -2,23 +2,26 @@ const Cardd= () => {
 
     return(
     <div  className="trmoney">
-<div className="col-auto"  >
+
+      
+<div className="col-auto"  id="one" >
+<div className="user">
     <label className="visually-hidden"  placeholder="Enter Your Username"for="autoSizingInputGroup"> Enter Your Username</label>
     <div className="input-group">
       <div className="input-group-text">@</div>
       <input type="text" className="form-control"   id="autoSizingInputGroup" placeholder="Username"></input>
       
-    </div>
-
-
-
-<br></br>
+    </div>  </div>
+     
+    <div className="row g-3">
+    
 
 
 
      
 <div className="row g-3">
 <br></br>
+
   <div className="col-sm-7">
  
     <input type="text" className="form-control" placeholder="Enter Full Name" aria-label="Enter Full Name"></input>
@@ -41,23 +44,27 @@ const Cardd= () => {
       <option>DENAR J</option>
     </select>
   </div>
-  
- 
-  <div className="col-md-6">
+  </div>
+ <div className="email">
+  <div className="col-md-7">
    
     <input type="email"  placeholder="Enter A Email"className="form-control" id="inputEmail4"></input>
-   <br></br>
+  
     </div>
-  </div>
-  <div className="col-md-6">
+    <div className="row g-1.5">
+    
+  </div></div>
+  <div className="bassw">
+  <div className="col-md-7">
    
     <input type="password"  placeholder="Enter Your Bassword" className="form-control" id="inputPassword4"></input>
-
+    </div> </div>
     <p> Agree To Our <a href="#">Terms & Privacy</a>.</p>
   </div>
-  <button type="button" className="btn btn-primary btn-sm">Sent</button>
-
-    
+<div className="btnn">
+  <button type="button" className="btn btn-outline-primary ">Sent</button>
+  
+</div> 
 </div>
 </div>
 )
