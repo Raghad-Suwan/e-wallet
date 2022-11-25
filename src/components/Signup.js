4fa3d18720375d17a761718738feba80567e5017
Malakmousa99 <116799883+Malakmousa99@users.noreply.github.com>
2022-11-25 21:59:56 +0200

    new Signup design

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,63 +1,157 @@
 
-import  React from 'react';
-import Grid from '@mui/material/Grid';
-import { Paper, Typography } from '@mui/material';
+import React from 'react';
 import Avatar from '@mui/material/Avatar';
-import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import { display } from '@mui/system';
-import Checkbox, { checkboxClasses } from '@mui/material/Checkbox';
-import FormGroup from '@mui/material/FormGroup';
-import  './Signup.css';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const Signup = () => {
+
+const theme = createTheme();
+
+ function Signup() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
 
   return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 25,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'text.disabled' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
 
-<Grid>
-<Grid  align='center'>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  variant="standard"
+                  autoFocus
+                />
+              </Grid>
 
-  <Grid className='avatarStyle'>
-  <Avatar>
-<PersonAddAlt1Icon/>
-</Avatar>
-  </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                  variant="standard"
+                />
 
-<h2 className='headerStyle'> Sign up </h2>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  variant="standard"
+                /> 
+              </Grid>
+             
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="country"
+                  label="Country"
+                  name="country"
+                  autoComplete="country"
+                  variant="standard"
+                /> 
+              </Grid>
 
-<Typography  variant='caption' gutterBottom>Please fill this form if you do not have account !</Typography>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="phone"
+                  label="Phone Number"
+                  type="phone"
+                  id="phone-num"
+                  autoComplete="phone"
+                  variant="standard"
+                />
+              </Grid>
 
-</Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  variant="standard"
+                />
+              </Grid>
 
-<form>
-<TextField   id="standard-basic" label="First name" variant="standard"  placeholder='Enter your first name' required  autoFocus  /> <br/>
-<TextField   id="standard-basic" label="Last name" variant="standard"  placeholder='Enter your last name'  required  /> <br/>
-<TextField  id="standard-basic" label="Email" variant="standard"  placeholder='Enter your email'   required  /> <br/>
-<TextField  id="standard-basic" label="Country" variant="standard"   placeholder='Enter your country'  required  /> <br/>
-<TextField  id="standard-basic" label="Phone number" variant="standard"   placeholder='Enter your phone number'   /> <br/>
-<TextField  id="standard-basic" label="Password" variant="standard"  placeholder='Enter your password'    required /> <br/> 
-<TextField  id="standard-basic" label="Confirm password" variant="standard"  placeholder='confirm password'    required/> <br/>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I agree to all Terms privacy policy and fees "
+                />
+              </Grid>
+            </Grid>
 
-<FormGroup className='formStyle '>
-      <FormControlLabel  control={<Checkbox defaultChecked  />} label="I accept to all terms and condition " />
-    </FormGroup>
-
-<Grid className='buttonStyle'>
-<Button type='Submit' variant='contained' color='inherit'  > Sign up </Button>
-</Grid>
-
-
-</form>
-
-</Grid>
-
-  )
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+    
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default Signup;
