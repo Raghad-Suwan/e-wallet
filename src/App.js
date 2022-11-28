8c54ea7204742e93917742ed2d29410e83f47588
izz-shakhsheer <117108297+izz-shakhsheer@users.noreply.github.com>
2022-11-28 21:16:30 +0200

    Push My code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import SignIn from './SignIn/index';
-import ForgotPassword from './ForgotPassword/index'
+import ForgotPassword from './ForgotPassword/index';
+import Text1 from './Pag1/Text1';
+import Text2 from  './Pag1/Text2'
+import Text3 from  './Pag1/Text3'
 
 function App() {
   return (
-    <SignIn />
-   //<ForgotPassword />
+  //   <SignIn />
+  //  //<ForgotPassword />
+    <div>
+      <Text1></Text1>
+      <Text2></Text2>
+      <Text3></Text3>
+    </div>
   );
 }
 
