455d4b1aef4bfe71af4acc03256281b3bfdd436d
Malakmousa99 <116799883+Malakmousa99@users.noreply.github.com>
2022-12-02 11:38:21 +0200

    update of contact page

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,91 +6,75 @@ import emailjs from 'emailjs-com';
 import 'react-toastify/dist/ReactToastify.min.css';
 import './ContactForm.css';
 
-const ContactForm = () => {
-    const {
-      register,
-      handleSubmit,
-      reset,
-      formState: { errors }
-    } = useForm();
-    const [disabled, setDisabled] = useState(false);
-  
-    // Function that displays a success toast on bottom right of the page when form submission is successful
-    const toastifySuccess = () => {
-      toast('Form sent!', {
-        position: 'bottom-right',
-        autoClose: 5000,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: false,
-        className: 'submit-feedback success',
-        toastId: 'notifyToast'
-      });
-    };
-  
-    // Function called on submit that uses emailjs to send email of valid contact form
-    const onSubmit = async (data) => {
-      // Destrcture data object
-      const { name, email, phonenumber, message } = data;
-      try {
-        // Disable form while processing submission
-        setDisabled(true);
-  
-        // Define template params
-        const templateParams = {
-          name,
-          email,
-          phonenumber,
-          message
-        };
-  
-        // Use emailjs to email contact form data
-        await emailjs.send(
-          process.env.REACT_APP_SERVICE_ID,
-          process.env.REACT_APP_TEMPLATE_ID,
-          templateParams,
-          process.env.REACT_APP_USER_ID
-        );
-  
-        // Reset contact form fields after submission
-        reset();
-        // Display success toast
-        toastifySuccess();
-        // Re-enable form submission
-        setDisabled(false);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-  
+
+ function ContactForm() {
+
+  const {
+    register, handleSubmit, reset, formState: { errors }
+  } = useForm();
+  const [disabled, setDisabled] = useState(false);
+
+  // Function that displays a success toast on bottom right of the page when form submission is successful
+  const toastifySuccess = () => {
+    toast('Form sent!', {
+      position: 'bottom-right',
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      className: 'submit-feedback success',
+      toastId: 'notifyToast'
+    });
+  };
+
+  // Function called on submit that uses emailjs to send email of valid contact form
+  const onSubmit = async (data) => {
+    // Destrcture data object
+    const { name, email, phonenumber, message } = data;
+    try {
+      // Disable form while processing submission
+      setDisabled(true);
+
+      // Define template params
+      const templateParams = {
+        name,
+        email,
+        phonenumber,
+        message
+      };
+
+      // Use emailjs to email contact form data
+      await emailjs.send(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        templateParams,
+        process.env.REACT_APP_USER_ID
+      );
+
+      // Reset contact form fields after submission
+      reset();
+      // Display success toast
+      toastifySuccess();
+      // Re-enable form submission
+      setDisabled(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
     return (
-      <>
-<header >
-      <div
-        className='p-5  hero-text  bg-image  header-img'
-    
-   >
-         <h1 className="h1-header mt-3 ">
-            Contact us
-          </h1>
-          <p>
-          Send your message and feedback to Jawwal Pay support 
-          and we will contact you as soon as possible!
-          </p>
-  
-</div>
-    </header>
-    <h3 className="h3-header mt-2">Contact us</h3>
-      <div className='ContactForm'>
+
+      <div className=' ContactForm'>
         <div className='container'>
           <div className='row'>
-            <div className='col-6 text-center'>
+            <div className=' col-6 text-center'>
+
               <div className='contactForm'>
                 <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
                   {/* Row 1 of form */}
                   <div className='row formRow'>
-                    <div className='col-6'>
+                    <div className=' col-xs-6 col-sm-6 col-lg-6 '>
                       <input
                         type='text'
                         name='name'
@@ -109,14 +93,13 @@ const ContactForm = () => {
                       ></input>
                       {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
                     </div>
-                    <div className='col-6'>
+                    <div className='col-xs-6 col-sm-6 col-lg-6'>
                       <input
                         type='email'
                         name='email'
                         {...register('email', {
                           required: true,
-                          pattern:
-                            /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                          pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
                         })}
                         className='form-control formInput2'
                         placeholder='Email address'
@@ -128,7 +111,7 @@ const ContactForm = () => {
                   </div>
                   {/* Row 2 of form */}
                   <div className='row formRow'>
-                    <div className='col-6'>
+                    <div className='col-xs-6 col-sm-6  col-lg-6'>
                       <input
                         type='tel'
                         name='phonenumber'
@@ -138,7 +121,7 @@ const ContactForm = () => {
                             message: 'Please enter a phone number'
                           },
                           maxLength: {
-                            value:10,
+                            value: 10,
                             message: 'phone number cannot be more 10 digit'
                           }
                         })}
@@ -151,10 +134,9 @@ const ContactForm = () => {
                     </div>
                   </div>
                   {/* Row 3 of form */}
-                  <div className='row formRow'>
-                    <div className='col'>
-                      <textarea 
-                        rows={3}
+                  <div className=' row formRow'>
+                    <div className='col-xs-12 col-sm-12 col-lg-12'>
+                      <textarea
                         name='message'
                         {...register('message', {
                           required: true
@@ -164,13 +146,12 @@ const ContactForm = () => {
                       ></textarea>
                       {errors.message && <span className='errorMessage'>Please enter a message</span>}
                     </div>
-             
+
                   </div>
 
-                  <button  data-bs-toggle="modal" data-bs-target="#myModal"   className='submit-btn' disabled={disabled} type='submit'>
+                  <button  className='submit-btn' disabled={disabled} type='submit'>
                     Send
                   </button>
-
                 </form>
               </div>
               <ToastContainer />
@@ -178,11 +159,9 @@ const ContactForm = () => {
           </div>
         </div>
       </div>
-      </>
-    );
-  };
   
+ 
+    );      
+  };
+
   export default ContactForm;
-
-
-
