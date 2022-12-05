969bfdc30e82ffdf9988cf720cf1f6123540ed92
Malakmousa99 <116799883+Malakmousa99@users.noreply.github.com>
2022-12-05 20:08:07 +0200

    update contact4

--- node_modules/ng-recaptcha/lib/recaptcha-settings.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="grecaptcha" />
-export interface RecaptchaSettings {
-    siteKey?: string;
-    theme?: ReCaptchaV2.Theme;
-    type?: ReCaptchaV2.Type;
-    size?: ReCaptchaV2.Size;
-    badge?: ReCaptchaV2.Badge;
-}
