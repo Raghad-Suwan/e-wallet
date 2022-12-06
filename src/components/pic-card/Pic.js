665bbe3307b4b51343e2969fcfb5437049624122
RaghadSuwan <raghadsu979@>
2022-12-06 17:24:23 +0200

    update some files in raghad

--- src/components/pic-card/Pic.js
+++ src/components/pic-card/Pic.js
@@ -6,7 +6,7 @@ import './Pic.css';
 const Pic=(props)=>{
     return (
 
-<div className=" container-fluid  justify-content-center">
+<div className=" all container-fluid  justify-content-center">
 <PicCard src={require('../../image/businessman.jpg')} title="you’r our special client"
 content="jcjdchbfbbbdccccbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbdhcdchios;skmsx" footer="know more about"/>
 <PicCard src={require('../../image/computer-laptop.jpg')} title="You know E-Wallet?"
