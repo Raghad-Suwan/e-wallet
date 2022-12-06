4ec2ec88ac8ae30563f893db723c8fae43bcc08f
deposit money <s12114975@stu.najah.edu>
2022-12-06 21:21:20 +0200

    services new

--- src/App.js
+++ src/App.js
@@ -1,47 +1,82 @@
+//***********  work malak 
 
-// import Adress from './Components/deposite/Adress';
-// import './Components/deposite/deposit.css';
-// import Card from './Components/deposite/Card';
+
+// import React, { Component } from 'react';
+// import PicCard from './components/PicCard';
+// import About from './components/About';
+// import './App.css';
+// import ContactForm from './components/ContactForm';
+// import Image from './components/Header';
+
+// import Signform from './components/Signup';
+
+
+//********** deposite
+
+
+// import Adress from './components/deposite/Adress';
+// import './components/deposite/deposit.css';
+// import Card from './components/deposite/Card' ;
+
+
+//********** services
+
 
 import './Components/services/services.css';
 import Cards from './Components/services/CardsServices ';
 import img1 from './Components/services/deposit1.jpg';
-import img2 from './Components/services/Transformation.jpg';
+import img2 from './Components/services/Transformation.jpg' ;
 import img3 from './Components/services/Withdrawal.jpg';
 import img4 from './Components/services/للدفع.jpg';
 import img5 from './Components/services/فاتورة.png';
 
+
 function App() {
-return (
-    <div className="container-fluid d-flex justify-content-center">
-                <section className="row">
-                    <div className="col-md-4">
-                        <Cards  imgsrc={img1} title="Money withdrawal" textt="You can withdraw any amount of money from your e-wallet at any time ... "/>
-                    </div>
-                    <div className="col-md-4">
-                        <Cards imgsrc={img2} title="Money transfer " textt="Transferring money to your family and friends is now easier, faster, and ..."/>
-                    </div>
-                    <div className="col-md-4">
-                        <Cards imgsrc={img3} title="Deposite money " textt="Feeding your account by depositing money into your e-wallet when visiting one of ..."/>
-                    </div>
-                    <div className="col-md-4">
-                        <Cards className='card44' imgsrc={img4} title="Pay for merchants" textt="Paying merchants, shops, and even restaurants is now easier and faster!..."/>
-                    </div>
-                
-                    <div className="col-md-4">
-                        <Cards imgsrc={img5} title="Pay bills" textt="Now, through your e-wallet, you can pay your bills and save them directly and.."/>
-                    </div>
-                
-                    
-                </section>
-            </div>
+  return (
+
+<div className="container-fluid d-flex justify-content-center">
+           <section className="row">
+               <div className="col-md-4">
+                   <Cards  imgsrc={img1} title="Money withdrawal" textt="You can withdraw any amount of money from your e-wallet at any time ... "/>
+               </div>
+              <div className="col-md-4">
+                   <Cards imgsrc={img2} title="Money transfer " textt="Transferring money to your family and friends is now easier, faster, and ..."/>
+               </div>
+               <div className="col-md-4">
+                   <Cards imgsrc={img3} title="Deposite money " textt="Feeding your account by depositing money into your e-wallet when visiting one of ..."/>
+               </div>
+               
+               <div className="card44 col-md-4">
+                   <Cards className='card44' imgsrc={img4} title="Pay for merchants" textt="Paying merchants, shops, and even restaurants is now easier and faster!..."/>
+               </div>
+           
+               <div className="col-md-4">
+                   <Cards imgsrc={img5} title="Pay bills" textt="Now, through your e-wallet, you can pay your bills and save them directly and.."/>
+               </div>
+             
+               
+          </section>
+       </div>  
+
+
+//********** work malak
+//     <div className="App">
+//   <Image/>
+//   <ContactForm/>
+
+
+// </div>
+
+//********** deposite
+
 //     <div className='app'>
     
 //     <Adress />
 //     <Card   />
     
 // </div>
-);
-}
 
+  );
+}
 export default App;
+
--- src/Components/deposite/Card.js
+++ src/Components/deposite/Card.js
@@ -1,60 +1,60 @@
-    import React from 'react'
-    import Button from './Button'
-    import Titlee from './Tittle'
-    // import Picture from './Picture'
+import React from 'react'
+import Button from './Button'
+import Titlee from './Titlee'
+// import Picture from './Picture'
 
-    //function 
+//function 
 
 const Card = (props) =>{
-    return(
-        <div>
-            {/* <Picture /> */}
-            <div className='container' >
-        
-
-    <div className="card">
+return(
+    <div>
+        {/* <Picture /> */}
+        <div className='container' >
     
+
+<div className="card">
+    <Titlee />
+
 <div className="card-body">
-<Titlee />
+<h6 className="text-name " >Wallet_ID</h6>
 
-    <h6 className="text-name text-white" >Wallet_ID</h6>
+<div className="input-group mb-3">
+<input type="number" class="form-control" placeholder="userID" aria-label="userID" />
+</div>
+<h6 className="text-name " >Agent's email</h6>
 
-    <div className="input-group mb-3">
-    <input type="number" class="form-control" placeholder="userID" aria-label="userID" />
-    </div>
-    <h6 className="text-name text-white" >Agent's email</h6>
+<div className="input-group mb-3">
+    <input type="text" class="form-control" placeholder="hadisawalmeh123"  aria-describedby="basic-addon2" />
+    <span class="input-group-text text-white" id="basic-addon2">@gmail.com</span>
+</div>
 
-    <div className="input-group mb-3">
-        <input type="text" class="form-control" placeholder="example@gmail.com"  aria-describedby="basic-addon2" />
-        <span class="input-group-text text-white" id="basic-addon2">@gmail.com</span>
-    </div>
+<h6 className="text-name " >Amount of money</h6> 
 
-    <h6 className="text-name text-white" >Amount of money</h6> 
+<div className="input-group mb-3">
+<input type="number" class="form-control" placeholder="Amount to be deposited" />
+<span className="input-group-text text-white">.00</span> 
+</div>
 
-    <div className="input-group mb-3">
-    <input type="number" class="form-control" placeholder="Amount to be deposited" />
-    <span className="input-group-text text-white">.00</span> 
-    </div>
+<h6 className='text-name '>currency type</h6>
 
-    <h6 className='text-name text-white'>currency type</h6>
-
-    <div className="input-group mb-3">
-    <select class="form-select" aria-label="Default select example">
-    <option value="1">dollar</option>
-    <option value="2">dinar</option>
-    <option value="3">shekel</option>
+<div className="input-group mb-3">
+<select class="form-select" aria-label="Default select example">
+<option value="1">dollar</option>
+<option value="2">dinar</option>
+<option value="3">shekel</option>
 </select>
-    </div>
-    <Button />
+</div>
+<Button />
 </div>
 </div>
 
 </div>
+
 </div>
+
 
     
-        
-    )
+)
 }
 export default Card
 
