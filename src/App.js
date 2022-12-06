784c07d6b6c8c1d217d17afb8c470be58ab5db59
Malakmousa99 <116799883+Malakmousa99@users.noreply.github.com>
2022-12-06 21:44:44 +0200

    new update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import React from 'react';
-//import PicCard from './component/PicCard';
-//import About from './component/About';
-//import './App.css';
-//import Signform from './components/Signup';
+import PicCard from './component/PicCard';
+import About from './component/About';
+import './App.css';
+import Signup from './components/Signup';
 import ContactForm from './components/ContactForm';
 import Image from './components/Header';
 import './components/ContactForm.css';
