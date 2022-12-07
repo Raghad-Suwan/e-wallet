04cbcd7efa88d7410a5fde107285627a675972b2
RaghadSuwan <raghadsu979@>
2022-12-07 15:15:09 +0200

    raghad befor merge

--- src/App.js
+++ src/App.js
@@ -1,93 +1,30 @@
 import React from "react";
 
 import "./App.css";
-<<<<<<< HEAD
-import "./services.css";
-=======
 // import "./services.css";
->>>>>>> 211fa0aeebcb2c55f7e3382ed7a958835750d5b0
 // import Cards from "./Components/CardsServices ";
 // import img1 from "./Components/deposit1.jpg";
 // import img2 from "./Components/Transformation.jpg";
 // import img3 from "./Components/Withdrawal.jpg";
 // import img4 from "./Components/للدفع.jpg";
 // import img5 from "./Components/فاتورة.png";
-<<<<<<< HEAD
-import Pic from "./components/pic-card/Pic"
-
-//   <Image/>
-//  <ContactForm/>
- 
-
-function App() {
-  return (
-    <Pic/>
-    // <div className="container-fluid d-flex justify-content-center">
-    //   <section className="row">
-    //     <div className="col-md-4">
-    //       <Cards
-    //         imgsrc={img1}
-    //         title="Money withdrawal"
-    //         textt="You can withdraw any amount of money from your e-wallet at any time ... "
-    //       />
-    //     </div>
-    //     <div className="col-md-4">
-    //       <Cards
-    //         imgsrc={img2}
-    //         title="Money transfer "
-    //         textt="Transferring money to your family and friends is now easier, faster, and ..."
-    //       />
-    //     </div>
-    //     <div className="col-md-4">
-    //       <Cards
-    //         imgsrc={img3}
-    //         title="Deposite money "
-    //         textt="Feeding your account by depositing money into your e-wallet when visiting one of ..."
-    //       />
-    //     </div>
-    //     <div className="col-md-4">
-    //       <Cards
-    //         className="card44"
-    //         imgsrc={img4}
-    //         title="Pay for merchants"
-    //         textt="Paying merchants, shops, and even restaurants is now easier and faster!..."
-    //       />
-    //     </div>
-    //     <div className="col-md-4">
-    //       <Cards
-    //         imgsrc={img5}
-    //         title="Pay bills"
-    //         textt="Now, through your e-wallet, you can pay your bills and save them directly and.."
-    //       />
-    //     </div>
-    //   </section>
-    // </div>
-
-=======
 
 
-
-import Signup from './components/Signup';
-import ContactForm from './components/ContactForm';
-import Image from './components/Header';
-import './components/ContactForm.css';
-
-function App() {
-  return (
-    <div className="App">
-  <Image/>
- <ContactForm/>
 
 // import Signup from './components/Signup';
 // import ContactForm from './components/ContactForm';
 // import Image from './components/Header';
 // import './components/ContactForm.css';
+
 //import Navbar from './Components/Navbar';
 // import ContactForm from './components/Con
 
  
+function App() {
+  return (
+    <div className="App">
+  
 </div>
->>>>>>> 211fa0aeebcb2c55f7e3382ed7a958835750d5b0
   );
 }
 export default App;
