42276aae7026c91ee35a278f91449f76384cba40
inas yaseen <s12112019@stu.najah.edu>
2022-12-09 23:19:13 +0200

    updat on searchbar

--- src/App.js
+++ src/App.js
@@ -1,76 +1,24 @@
 import React from "react";
-<<<<<<< HEAD
-import { Nav } from "react-bootstrap";
-import{BrowserRouter, Routes, Route} from 'react-router-dom';
-
-//import "./App.css";
-//import "./components/services/services.css";
-
-
-//import Signup from './components/Signup';
-//import ContactForm from './components/ContactForm';
-//import Image from './components/Header';
-//import './components/ContactForm.css';
-// import "./services.css";
-
-// import Cards from "./Components/CardsServices ";
-// import img1 from "./Components/deposit1.jpg";
-// import img2 from "./Components/Transformation.jpg";
-// import img3 from "./Components/Withdrawal.jpg";
-// import img4 from "./Components/للدفع.jpg";
-// import img5 from "./Components/فاتورة.png";
-import Payment from "./components/marchent-bilss/Payment";
-import PayMentOfBills from "./components/marchent-bilss/PayMentOfBills";
-import StepsToPay from "./components/marchent-bilss/StepsToPay";
-//import PAY from "./components/marchent-bilss/Pay";
-//import Shopping from "./components/marchent-bilss/Shopping";
-//import STEPS from "./components/marchent-bilss/Steps";
-//import SearchBar from "./components/searchbar/SearchBar";
-
-
-=======
 import "./App.css";
-import{BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './components/Home/Home';
-import ContactForm from './components/Contact/ContactForm';
-import MonyTransferPage from './components/Money-Transfer/MonyTransferPage';
-import MarchentBills from './components/Marchent-bilss/MarchentBills';
-import MobileTopUpPage from './components/Mobile-Top-Up/MobileTopUpPage';
-import CashInthoughAgents from './components/Cash-In-though-Agents/CashInthoughAgents';
-import Login from './components/Signing/Login';
-import Signup from './components/Signing/Signup';
-import AboutPage from './components/About/AboutPage';
-import Card from "./components/Deposite/Card";
-import Card3 from "./components/Properties/Card3";
->>>>>>> 3a6eaa73b81a9a5c117c6b8119edcc34fe9153a7
+//import{BrowserRouter, Routes, Route} from 'react-router-dom';
+//import Home from './components/Home/Home';
+//import ContactForm from './components/Contact/ContactForm';
+//import MonyTransferPage from './components/Money-Transfer/MonyTransferPage';
+//import MarchentBills from './components/Marchent-bilss/MarchentBills';
+//import MobileTopUpPage from './components/Mobile-Top-Up/MobileTopUpPage';
+//import CashInthoughAgents from './components/Cash-In-though-Agents/CashInthoughAgents';
+//import Login from './components/Signing/Login';
+//import Signup from './components/Signing/Signup';
+//import AboutPage from './components/About/AboutPage';
+//import Card from "./components/Deposite/Card";
+//import Card3 from "./components/Properties/Card3";
+import SearchBar from "./components/searchbar/SearchBar";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Payment />
-      <PayMentOfBills />
-      <StepsToPay />
-
+      <SearchBar />
     </div>
-=======
-      <BrowserRouter>
-        <Routes>
-        <Route path="/" index element={<Home />} />
-        <Route path="/sign" element ={<Signup />} />
-        <Route path="/Contact " element={<ContactForm />} />
-        <Route path="/about" element ={<AboutPage/>} />
-        <Route path="/login" element ={< Login/>} />
-
-        <Route path="/cashInthoughAgentst" element={<CashInthoughAgents />} />
-        <Route path="/trancfer" element={<MonyTransferPage/>} />
-        <Route path="/mobiltop" element={<MobileTopUpPage/>} />
-        <Route path="/bills" element={<MarchentBills/>} />
-      </Routes>
-     </BrowserRouter>
-     <Card3/>
-</div>
->>>>>>> 3a6eaa73b81a9a5c117c6b8119edcc34fe9153a7
   );
 }
 export default App;
--- src/Components/marchent-bilss/MarchentBills.js
+++ src/Components/marchent-bilss/MarchentBills.js
@@ -4,20 +4,12 @@ import './Buy.css';
 import PAY from './Pay';
 import PayMentOfBills from './PayMentOfBills';
 import Payment from './Payment.js';
-<<<<<<< HEAD:src/components/marchent-bilss/Conect.js
-import STEPS from './STEPS';
-=======
 import Steps from './Steps';
->>>>>>> 3a6eaa73b81a9a5c117c6b8119edcc34fe9153a7:src/Components/marchent-bilss/MarchentBills.js
 import StepsToPay from './StepsToPay';
 import Shopping from './Shopping';
 
 
-<<<<<<< HEAD:src/components/marchent-bilss/Conect.js
-export default function Conect() {
-=======
 export default function MarchentBills() {
->>>>>>> 3a6eaa73b81a9a5c117c6b8119edcc34fe9153a7:src/Components/marchent-bilss/MarchentBills.js
   return (
     <div>
       <PAY></PAY>
