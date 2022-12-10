ad5cce9c6fd7ff58b1a48e9b4566eebe821a0796
Malakmousa99 <116799883+Malakmousa99@users.noreply.github.com>
2022-12-10 19:55:42 +0200

    push code malak

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-//import "./App.css";
-//import{BrowserRouter, Routes, Route} from 'react-router-dom';
-//import Home from './components/Home/Home';
-//import ContactForm from './components/contact/ContactForm';
-//import MonyTransferPage from './components/Money-Transfer/MonyTransferPage';
-//import MarchentBills from './components/Marchent-bilss/MarchentBills';
-//import MobileTopUpPage from './components/Mobile-Top-Up/MobileTopUpPage';
-//import CashInthoughAgents from './components/Cash-In-though-Agents/CashInthoughAgents';
-//import Login from './components/Signing/Login';
+import "./App.css";
+import{BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home/Home';
+import ContactForm from './components/contact/ContactForm';
+import MonyTransferPage from './components/Money-Transfer/MonyTransferPage';
+import MarchentBills from './components/Marchent-bilss/MarchentBills';
+import MobileTopUpPage from './components/Mobile-Top-Up/MobileTopUpPage';
+import CashInthoughAgents from './components/Cash-In-though-Agents/CashInthoughAgents';
+import Login from './components/Signing/Login';
 import Signup from './components/Signing/Signup';
-//import AboutPage from './components/About/AboutPage';
+import AboutPage from './components/About/AboutPage';
 
 function App() {
   return (
     <div className="App">
 
-{/* 
+{
       <BrowserRouter>
         <Routes>
         <Route path="/" index element={<Home />} />
@@ -29,8 +29,7 @@ function App() {
         <Route path="/bills" element={<MarchentBills/>} />
       </Routes>
      </BrowserRouter>
-    <ContactForm/> */
-    <Signup/>
+
     
     }
 </div>
