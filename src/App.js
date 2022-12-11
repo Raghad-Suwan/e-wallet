24b0a0ec3deeb688dae0d600c1f286a52ab0ea01
inas yaseen <s12112019@stu.najah.edu>
2022-12-11 17:06:39 +0200

    task2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import "./App.css";
-<<<<<<< HEAD
 //import{BrowserRouter, Routes, Route} from 'react-router-dom';
 //import Home from './components/Home/Home';
 //import ContactForm from './components/Contact/ContactForm';
@@ -20,31 +19,10 @@ import Stepstopayingmarchent from "./components/Pay-to-Merchants/Stepstopayingma
 //import Paymentbills from "./components/Payment-of-bills/Paymentbills";
 //import StepsToPayment from "./components/Payment-of-bills/StepsToPayment";
 //mport SearchBar from "./components/searchbar/SearchBar";
-=======
-import{BrowserRouter, Routes, Route} from 'react-router-dom';
-import MonyTransferPage from './components/Pages/Money-Transfer/MonyTransferPage';
-import MarchentBills from './components/Pages/Marchent-bilss/MarchentBills';
-import MobileTopUpPage from './components/Pages/Mobile-Top-Up/MobileTopUpPage';
-import CashInthoughAgents from './components/Pages/Cash-In-though-Agents/CashInthoughAgents';
-import Login from './components/Signing/Login';
-import Signup from './components/Signing/Signup';
-import Home from "./components/Pages/Home/Home";
-import ContactForm from "./components/ContactUs/ContactForm";
-import AboutPage from "./components/Pages/About-Us/AboutPage";
-
-
->>>>>>> 34b1f5a4528e2f11aa6fcbb9d8faa72c49fe5e3f
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Hedermarchent></Hedermarchent>
-      <Payingmarchent></Payingmarchent>
-      <Stepstopayingmarchent></Stepstopayingmarchent>
-  
-    </div>
-=======
       <BrowserRouter>
         <Routes>
         <Route path="/" index element={<Home/>} />
@@ -61,7 +39,6 @@ function App() {
      
      
 </div>
->>>>>>> 34b1f5a4528e2f11aa6fcbb9d8faa72c49fe5e3f
   );
 }
 export default App;
