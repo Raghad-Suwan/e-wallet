b14faf2be21cbde221f24dd9ce09e3aed8538151
inas yaseen <s12112019@stu.najah.edu>
2022-12-15 19:26:56 +0200

    update on sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Menue from "./components/Menue/Menue";
+import Menue from "./components/WalletDesign/Menue";
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -2,37 +2,11 @@ import './index.css';
 import React from 'react';              
 import App from './App';                
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
-import Home from "./components/Pages/Home/Home";
-
-import MonyTransferPage from './components/Pages/Money-Transfer/MonyTransferPage';
-import MarchentBills from './components/Pages/Marchent-bilss/MarchentBills';
-import MobileTopUpPage from './components/Pages/Mobile-Top-Up/MobileTopUpPage';
-import CashInthoughAgents from './components/Pages/Cash-In-though-Agents/CashInthoughAgents';
-import Login from './components/Signing/Login';
-import Signup from './components/Signing/Signup';
-import ContactForm from "./components/ContactUs/ContactForm";
-import AboutPage from './components/Pages/About-Us/AboutPage';
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
          <App/>
-     <BrowserRouter>
-        <Routes>
-        <Route path="/" index element={<Home/>} />
-        <Route path="/sign" element ={<Signup />} />
-        <Route path="/Contact " element={<ContactForm/>} />
-        <Route path="/about" element ={<AboutPage/>} />
-        <Route path="/login" element ={< Login/>} />
-        <Route path="/cashInthoughAgentst" element={<CashInthoughAgents/>} />
-        <Route path="/trancfer" element={<MonyTransferPage/>} />
-        <Route path="/mobiltop" element={<MobileTopUpPage/>} />
-        <Route path="/bills" element={<MarchentBills/>} />
-        </Routes>
-      </BrowserRouter>
   </React.StrictMode>
 );
 
