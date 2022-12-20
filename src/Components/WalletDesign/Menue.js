8727f81534c506ac0d38765e605cb3525bda2db7
lara-taweel <s12115919@stu.najah.edu>
2022-12-20 17:19:37 +0300

    edit

--- src/Components/WalletDesign/Menue.js
+++ src/Components/WalletDesign/Menue.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Menue.css';
-<<<<<<< HEAD
 export default function Menue() {
   return (
    <div>
@@ -41,43 +40,6 @@ export default function Menue() {
             </li><li className="item">
             <a className="nav-link " aria-current="page" href="index.html"><i className='bi-box-arrow-in-right'></i>log out</a>
           </li>
-=======
-
-export default function Menue() {
-  return (
-    <div>
-      <nav class="navbar navbar-dark bg-dark fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.html"><i className='bi-person-circle'>Karma astro</i></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">menu</h5>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.html"><i className='bi bi-wallet2'></i>My wallet</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.html"><i className='bi-person-bounding-box'></i>My account</a>
-          </li><li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.html"><i className='bi-box-arrow-in-right'></i>log out</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="index.html" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i className='bi-bag-plus-fill'></i>Serves
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark">
-              <li><a class="dropdown-item" href="index.html">withdraw money</a></li>
-              <li><a class="dropdown-item" href="index.html">deposit money</a></li>
-              <li><a class="dropdown-item" href="index.html">mobile top-up</a></li>
-              <li><a class="dropdown-item" href="index.html">money transfer</a></li>
-            </ul>
->>>>>>> d8465d823a64c45ba3d2c076d4e753d4c605b781
           </li>
         </ul>
       </div>
