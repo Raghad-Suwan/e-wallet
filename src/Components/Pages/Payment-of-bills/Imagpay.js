c63cf724c152d8eaf8a3521bbe70ad4c992791d9
inas yaseen <s12112019@stu.najah.edu>
2022-12-22 00:03:40 +0200

    updat on responsive in payment of bilss page

---
+++ src/Components/Pages/Payment-of-bills/Imagpay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Imagpay() {
+  return (
+    <div className='img-pay'>
+    
+            <img className="img-fluid mobile-app" src={require("../../../images/payment.png")}  alt="mobile-respon">
+            </img>
+           
+    </div>
+  )
+}
