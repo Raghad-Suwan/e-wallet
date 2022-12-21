b97e76dc95e549cc2d8e26acbb137e6095908d00
lara-taweel <s12115919@stu.najah.edu>
2022-12-21 14:14:48 +0300

    transfer final edit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
-import Transfer from "./components/Transfer/Transfer";
-import ImportWithdraw from "./components/Withdrawa_money/ImportWithdraw";
-
 
 function App() {
   return (
     <>
-   < ImportWithdraw/>
+  
    </>
   );
 }
