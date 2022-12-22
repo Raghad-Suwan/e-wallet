29c699beda00fce4d52ce6231652363d3c851b20
inas yaseen <s12112019@stu.najah.edu>
2022-12-23 00:29:24 +0200

    updat on paybiils page

--- src/Components/Pages/Payment-of-bills/StepsToPayment.js
+++ src/Components/Pages/Payment-of-bills/StepsToPayment.js
@@ -1,21 +1,19 @@
 import React from 'react'
-import './Mentt.css';
-
 export default function StepsToPayment() {
   return (
-    <div >
-      <h1 className='HEAD pt-4'>STEPS TO PAY BILLS:</h1>
-      <ul className='descrip pb-5'>
-        <li>Select “Pay Bills” from the main menu of your e-wallet.</li>
-        <li>Select the category, for example: Telecomes</li>
-        <li>From the category you selected, search for the company  service whose bill you wish to pay.</li>
-        <li>After selecting the company  service, enter the reference number of the bill you wish to pay.</li>
-        <li>You can save the company service you selected and whose reference number you entered to use it later. </li>
-        <li>From the bills list of each company, choose the unpaid bill. </li>
-        <li>Select “Pay Bill”. </li>
-        <li>After the bill details are displayed, and in order to confirm payment, press “Confirm Transaction”. </li>
-        <li>You will receive a confirmation of the transaction success and an image of the receipt. </li>
-      </ul>
+    <div className='descrip'>
+       <h1 className='HEADofthepage' >STEPS TO PAY BILLS:</h1>
+ <ul>
+    <li>Select “Pay Bills” from the main menu of your e-wallet.</li>
+    <li>Select the category, for example: Telecomes</li>
+    <li>From the category you selected, search for the company / service whose bill you wish to pay.</li>
+    <li>After selecting the company / service, enter the reference number of the bill you wish to pay.</li>
+    <li>You can save the company service you selected and whose reference number you entered to use it later. </li>
+    <li>From the bills list of each company, choose the unpaid bill. </li>
+    <li>Select “Pay Bill”. </li>
+    <li>After the bill details are displayed, and in order to confirm payment, press “Confirm Transaction”. </li>
+    <li>You will receive a confirmation of the transaction success and an image of the receipt. </li>
+ </ul>
     </div>
   )
 }
