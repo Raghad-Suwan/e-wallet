8505ad1ed146f824197da975f3c38d9f9600bcd1
inas yaseen <s12112019@stu.najah.edu>
2022-12-22 20:06:25 +0200

    updat on header

--- src/Components/Pages/Pay-to-Merchants/Hedermarchent.js
+++ src/Components/Pages/Pay-to-Merchants/Hedermarchent.js
@@ -4,7 +4,7 @@ export default function Hedermarchent() {
   return ( 
   <div className='imag-marchent-inas'>
     <div className="i-left">
-      <span>Pay to Merchants <br></br>
+      <span>Pay to Merchant <br></br>
       Pay your bills at stores and cafes with E walle </span>
     </div>
     <img className="MOBILE-ApP" src={require("../../../images/marchent.png")}  alt="iphon+13"></img>
