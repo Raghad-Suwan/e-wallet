9eaacc998003347d566b6bc9354a80f578f2c280
lara-taweel <s12115919@stu.najah.edu>
2022-12-23 05:00:25 +0300

    edit2525

--- src/Components/Transfer/Adress.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const AdressTransfer=()=>{
-    return(
-        <div className ="walllet-contaeiner text-center">
-        <h1 className="adressss text-dark ">welcome all , have a nice day</h1>
-        </div>
-        )
-    }
-    export default AdressTransfer;
